70eae952f29105216b08d7eda74cb96588c21967
Jesse Moses <jessemoses71@gmail.com>
2023-08-16 17:00:03 +0100

    Added space between header lines

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,8 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover & Share
-        <br className="max-md:hidden" />
+        {/* <br className="max-md:hidden" /> */}
+        <br />
         <span className="orange_gradient text-center">AI Powered Prompts</span>
       </h1>
       <p className="desc text-center">
